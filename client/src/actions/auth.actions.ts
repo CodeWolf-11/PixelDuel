570337892c72772daa4292831cec44ac5c5443ce
Nishant Rai <n.rai.code@outlook.com>
2024-09-11 10:56:55 +0530

    reset password implemented

--- client/src/actions/auth.actions.ts
+++ client/src/actions/auth.actions.ts
@@ -1,7 +1,7 @@
 "use server"
 
-import { CHECK_CREDENTIALS_URL, FORGET_PASSWORD_URL, REGISTER_URL } from "@/lib/apiEndpoints"
-import axios, { AxiosError } from "axios"
+import { CHECK_CREDENTIALS_URL, FORGET_PASSWORD_URL, REGISTER_URL, RESET_PASSWORD_URL } from "@/lib/apiEndpoints"
+import axios, { Axios, AxiosError } from "axios"
 
 export const registerActions = async (prevState: any, formdata: FormData) => {
     try {
@@ -110,6 +110,45 @@ export const forgetPassordAction = async (prevState: any, formdata: FormData) =>
             status: 500,
             message: "Something went wrong",
             errors: {},
+        }
+    }
+}
+
+
+export const resetPasswordAction = async (prevState: any, formdata: FormData) => {
+    try {
+
+        const { data } = await axios.post(RESET_PASSWORD_URL, {
+            email: formdata.get("email"),
+            password: formdata.get("password"),
+            confirm_password: formdata.get("confirm_password"),
+            token: formdata.get("token")
+        });
+
+        return {
+            status: 200,
+            message: data?.message ?? "Password updated successfully",
+            errors: {},
+        }
+
+    } catch (error) {
+
+        if (error instanceof AxiosError) {
+
+            if (error.response?.status == 422) {
+
+                return {
+                    status: 422,
+                    message: error.response?.data?.message ?? "Inavlid data",
+                    errors: error.response.data.errors,
+                }
+            }
+        }
+
+        return {
+            status: 500,
+            message: "Something went wrong",
+            errors: {}
         }
     }
 }
