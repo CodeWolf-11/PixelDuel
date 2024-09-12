c7606f8e1a94dc45000136e57f3cb1970a038b45
Nishant Rai <n.rai.code@outlook.com>
2024-09-12 14:05:42 +0530

    git bug fix 1

--- server/dist/controllers/user.controllers.js
+++
@@ -1,6 +0,0 @@
-export const getUserController = async (req, res) => {
-    const user = req.user;
-    return res.json({
-        data: user
-    });
-};
--- server/dist/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import authMiddleware from "../middleware/auth.middleware.js";
-import { getUserController } from "../controllers/user.controllers.js";
-const router = Router();
-router.get("/", authMiddleware, getUserController);
-export default router;
