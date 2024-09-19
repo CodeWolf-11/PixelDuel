6b04f4f00a3de83b2c5572390fdbeb6f2b0a482a
Nishant Rai <n.rai.code@outlook.com>
2024-09-19 10:56:57 +0530

    Dockerfile added for both client and server

--- client/src/app/(auth)/reset-password/page.tsx
+++ client/src/app/(auth)/reset-password/page.tsx
@@ -1,13 +1,14 @@
 import ResetPasswordForm from "@/components/auth/ResetPassword";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Page: React.FC = () => {
     return <div className="h-screen w-full flex justify-center items-center p-2">
         <div className="w-[550px] bg-white rounded-xl shadow-lg py-5 px-10">
             <h1 className="text-4xl text-center font-extrabold bg-gradient-to-r from-green-300 to-green-700 text-transparent bg-clip-text" >PixelDuel</h1>
             <h1 className="text-3xl font-bold">Reset Password</h1>
-
-            <ResetPasswordForm />
+            <Suspense>
+                <ResetPasswordForm />
+            </Suspense>
         </div>
     </div>
 }
